1b7146e1878fe5ba6fc10905fa9c88b3a4f1b124
Cristóbal Moreno <crismo1712@uc.cl>
2024-09-11 20:51:31 -0300

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -16,8 +16,12 @@ module.exports = {
     }
   },
   production: {
-    use_env_variable: 'DATABASE_URL',
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
     dialect: 'postgres',
+    port: process.env.DB_PORT,
     dialectOptions: {
       ssl: {
         require: true,
